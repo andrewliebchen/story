74b1da7fab4fef4abb4816d3fee9819513b72438
Andrew Liebchen <liebchen@fb.com>
2020-09-01 02:17:45 -0700

    Time formatting, and some other things

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,11 +1,18 @@
 import { Box, Button, Flex, Heading, Textarea, Text } from "theme-ui";
 import { useTracker } from "meteor/react-meteor-data";
+import en from "javascript-time-ago/locale/en";
 import MarkdownView from "react-showdown";
 import React, { useState } from "react";
 import Thoughts from "../api/thoughts";
+import TimeAgo from "react-time-ago";
+import JavascriptTimeAgo from "javascript-time-ago";
+
+JavascriptTimeAgo.addLocale(en);
 
 function App() {
-  const thoughts = useTracker(() => Thoughts.find({}).fetch());
+  const thoughts = useTracker(() =>
+    Thoughts.find({}, { sort: { createdAt: -1 } }).fetch()
+  );
   const [value, setValue] = useState("");
 
   return (
@@ -15,6 +22,7 @@ function App() {
           defaultValue={value}
           onChange={event => setValue(event.target.value)}
           mb={3}
+          placeholder="Add a thought"
         />
         <Button
           variant="primary"
@@ -23,6 +31,7 @@ function App() {
               {
                 value: value,
                 createdAt: Date.now(),
+                lastUpdated: Date.now(),
                 done: false
               },
               (err, success) => success && console.log("Added")
@@ -37,15 +46,22 @@ function App() {
         {thoughts.map(thought => (
           <Flex key={thought._id} sx={{ alignItems: "center", mt: 3 }}>
             <Box>
-              <Text variant="secondary">{thought.createdAt}</Text>
               <Text sx={{ textDecoration: thought.done && "line-through" }}>
                 <MarkdownView markdown={thought.value} />
               </Text>
+              <Text variant="secondary">
+                Created <TimeAgo date={thought.createdAt} />
+              </Text>
+              {thought.updatedAt && (
+                <Text variant="secondary">
+                  Updated <TimeAgo date={thought.updatedAt} />
+                </Text>
+              )}
               <Button
-                children={thought.done ? "Not done" : "Done"}
+                children={thought.done ? "Undo" : "Done"}
                 onClick={() =>
                   Thoughts.update(thought._id, {
-                    $set: { done: !thought.done }
+                    $set: { done: !thought.done, updatedAt: Date.now() }
                   })
                 }
               />
