5d181fd57375d7fce2568beaaa0ef0b794655673
Andrew Liebchen <liebchen@fb.com>
2020-09-03 21:54:13 -0700

    Stub in story UI

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -24,81 +24,91 @@ function App() {
   const [value, setValue] = useState("");
 
   return (
-    <Box p={3}>
-      <Box p={3}>
-        <Textarea
-          defaultValue={value}
-          onChange={event => setValue(event.target.value)}
-          mb={3}
-          placeholder="Add a thought"
-        />
-        <Button
-          variant="primary"
-          onClick={() =>
-            Thoughts.insert(
-              {
-                value: value,
-                createdAt: Date.now(),
-                lastUpdated: Date.now(),
-                done: false
-              },
-              (err, success) => success && console.log("Added")
-            )
-          }
-        >
-          Create
-        </Button>
+    <Box
+      sx={{
+        display: "grid",
+        gridGap: 3,
+        gridTemplateColumns: "1fr 400px",
+        p: 3
+      }}
+    >
+      <Box>
+        <Textarea placeholder="Write your story" sx={{ height: "50vh" }} />
       </Box>
-      <Box p={3}>
-        <Heading>Thoughts</Heading>
-        {thoughts.map(thought => (
-          <Flex
-            key={thought._id}
-            sx={{
-              alignItems: "center",
-              justifyContent: "space-between",
-              mx: -3,
-              p: 3,
-              "&:hover": {
-                backgroundColor: "muted"
-              }
-            }}
+      <Box>
+        <Box>
+          <Textarea
+            defaultValue={value}
+            onChange={event => setValue(event.target.value)}
+            mb={3}
+            placeholder="Add a thought"
+          />
+          <Button
+            variant="primary"
+            onClick={() =>
+              Thoughts.insert(
+                {
+                  value: value,
+                  createdAt: Date.now(),
+                  lastUpdated: Date.now(),
+                  done: false
+                },
+                (err, success) => success && console.log("Added")
+              )
+            }
           >
-            <Box>
-              <Text sx={{ textDecoration: thought.done && "line-through" }}>
-                <MarkdownView markdown={thought.value} />
-              </Text>
-              <Text variant="secondary">
-                Created <TimeAgo date={thought.createdAt} />
-              </Text>
-              {thought.updatedAt && (
-                <Text variant="secondary">
-                  Updated <TimeAgo date={thought.updatedAt} />
+            Create
+          </Button>
+          <Heading mt={3}>Thoughts</Heading>
+          {thoughts.map(thought => (
+            <Flex
+              key={thought._id}
+              sx={{
+                alignItems: "center",
+                justifyContent: "space-between",
+                mx: -3,
+                p: 3,
+                "&:hover": {
+                  backgroundColor: "muted"
+                }
+              }}
+            >
+              <Box>
+                <Text sx={{ textDecoration: thought.done && "line-through" }}>
+                  <MarkdownView markdown={thought.value} />
                 </Text>
-              )}
-            </Box>
-            <Flex sx={{ alignItems: "center" }}>
-              <IconButton
-                mr={2}
-                onClick={() =>
-                  window.confirm(
-                    "Are you sure you want to delete this Thought? This action can't be undone."
-                  ) && Thoughts.remove(thought._id)
-                }
-              >
-                🗑
-              </IconButton>
-              <Button
-                children={thought.done ? "Open" : "Resolve"}
-                onClick={() =>
-                  Thoughts.update(thought._id, {
-                    $set: { done: !thought.done, updatedAt: Date.now() }
-                  })
-                }
-              />
+                <Text variant="secondary">
+                  Created <TimeAgo date={thought.createdAt} />
+                </Text>
+                {thought.updatedAt && (
+                  <Text variant="secondary">
+                    Updated <TimeAgo date={thought.updatedAt} />
+                  </Text>
+                )}
+              </Box>
+              <Flex sx={{ alignItems: "center" }}>
+                <IconButton
+                  mr={2}
+                  onClick={() =>
+                    window.confirm(
+                      "Are you sure you want to delete this Thought? This action can't be undone."
+                    ) && Thoughts.remove(thought._id)
+                  }
+                >
+                  🗑
+                </IconButton>
+                <Button
+                  children={thought.done ? "Open" : "Resolve"}
+                  onClick={() =>
+                    Thoughts.update(thought._id, {
+                      $set: { done: !thought.done, updatedAt: Date.now() }
+                    })
+                  }
+                />
+              </Flex>
             </Flex>
-          </Flex>
-        ))}
+          ))}
+        </Box>
       </Box>
     </Box>
   );
--- server/main.js
+++ server/main.js
@@ -1,27 +1,3 @@
 import { Meteor } from "meteor/meteor";
 import Thoughts from "/imports/api/thoughts";
-
-Meteor.startup(() => {
-  // // If the Links collection is empty, add some data.
-  // if (LinksCollection.find().count() === 0) {
-  //   insertLink({
-  //     title: 'Do the Tutorial',
-  //     url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-  //   });
-  //
-  //   insertLink({
-  //     title: 'Follow the Guide',
-  //     url: 'http://guide.meteor.com'
-  //   });
-  //
-  //   insertLink({
-  //     title: 'Read the Docs',
-  //     url: 'https://docs.meteor.com'
-  //   });
-  //
-  //   insertLink({
-  //     title: 'Discussions',
-  //     url: 'https://forums.meteor.com'
-  //   });
-  // }
-});
+import Stories from "/imports/api/stories";
