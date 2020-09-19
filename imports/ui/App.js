a51f37397ff3b6cd954d7ed8f88285778fe1537a
Andrew Liebchen <liebchen@fb.com>
2020-09-19 16:05:57 -0700

    Split out Chapters component

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,7 +2,7 @@ import { Box, Checkbox, Flex, Grid, Heading, IconButton, Text } from "theme-ui";
 import { Meteor } from "meteor/meteor";
 import { wordChunks } from "split-word";
 import AppContext from "./AppContext";
-import Chapters from "../api/chapters";
+import Chapters from "./Chapters";
 import ElementRow from "./ElementRow";
 import MarkdownView from "react-showdown";
 import New from "./New";
@@ -10,116 +10,69 @@ import React from "react";
 import Thoughts from "../api/thoughts";
 import Word from "./Word";
 
-function App() {
-  return (
-    <AppContext.Consumer>
-      {(props) => (
-        <Grid
-          sx={{ p: 4, gap: 4, gridAutoFlow: "column", gridAutoColumns: "1fr" }}
-        >
-          <Box>
-            <Heading mb={3}>Chapters</Heading>
-            <New
-              clickMixin={(id, value) =>
-                wordChunks(value).map((word, index) =>
-                  Meteor.call("words.insert", {
-                    value: word,
-                    index: index,
-                    parentId: id,
-                    createdAt: Date.now(),
-                    updatedAt: Date.now(),
-                  })
-                )
-              }
-              cursor={Chapters}
-              placeholder="Add a new chapter"
-            />
-            {props.chapters &&
-              props.chapters.map((chapter) => (
+const App = () => (
+  <AppContext.Consumer>
+    {(props) => (
+      <Grid
+        sx={{ p: 4, gap: 4, gridAutoFlow: "column", gridAutoColumns: "1fr" }}
+      >
+        <Chapters />
+        <Box>
+          <Heading mb={3}>Thoughts</Heading>
+          <New cursor={Thoughts} placeholder="Add a new thought" />
+          <Flex
+            sx={{ cusor: "pointer", mt: 3 }}
+            onClick={() => props.setShowMuted(!props.showMuted)}
+          >
+            <Checkbox checked={props.showMuted} readOnly />
+            <Text>Show muted thoughts</Text>
+          </Flex>
+          {props.thoughts &&
+            props.thoughts
+              .filter((thought) => (props.showMuted ? !thought.done : true))
+              .map((thought) => (
                 <ElementRow
-                  key={chapter._id}
+                  key={thought._id}
                   content={
-                    <Flex>
-                      {props.words
-                        .filter((word) => word.parentId === chapter._id)
-                        .map((word) => (
-                          <Word key={word._id} words={props.words} {...word} />
-                        ))}
-                    </Flex>
+                    <Text
+                      sx={{
+                        textDecoration: thought.done && "line-through",
+                        flex: "auto",
+                      }}
+                    >
+                      <MarkdownView markdown={thought.value} />
+                    </Text>
                   }
+                  isMuted={props.done}
                   actions={
-                    <IconButton
-                      children="🗑"
-                      mr={2}
-                      onClick={() =>
-                        window.confirm(
-                          "Are you sure you want to delete this chapter? This action can't be undone."
-                        ) && Meteor.call("chapters.remove", chapter._id)
-                      }
-                    />
+                    <>
+                      <IconButton
+                        children="🗑"
+                        mr={2}
+                        onClick={() =>
+                          window.confirm(
+                            "Are you sure you want to delete this Thought? This action can't be undone."
+                          ) && Meteor.call("thoughts.remove", thought._id)
+                        }
+                      />
+                      <IconButton
+                        children={thought.done ? "🛑" : "✅"}
+                        onClick={() =>
+                          Meteor.call("thoughts.update", thought._id, {
+                            done: !thought.done,
+                            updatedAt: Date.now(),
+                          })
+                        }
+                      />
+                    </>
                   }
-                  {...chapter}
+                  {...thought}
                 />
               ))}
-          </Box>
-
-          <Box>
-            <Heading mb={3}>Thoughts</Heading>
-            <New cursor={Thoughts} placeholder="Add a new thought" />
-            <Flex
-              sx={{ cusor: "pointer", mt: 3 }}
-              onClick={() => props.setShowMuted(!props.showMuted)}
-            >
-              <Checkbox checked={props.showMuted} readOnly />
-              <Text>Show muted thoughts</Text>
-            </Flex>
-            {props.thoughts &&
-              props.thoughts
-                .filter((thought) => (props.showMuted ? !thought.done : true))
-                .map((thought) => (
-                  <ElementRow
-                    key={thought._id}
-                    content={
-                      <Text
-                        sx={{
-                          textDecoration: thought.done && "line-through",
-                          flex: "auto",
-                        }}
-                      >
-                        <MarkdownView markdown={thought.value} />
-                      </Text>
-                    }
-                    isMuted={props.done}
-                    actions={
-                      <>
-                        <IconButton
-                          children="🗑"
-                          mr={2}
-                          onClick={() =>
-                            window.confirm(
-                              "Are you sure you want to delete this Thought? This action can't be undone."
-                            ) && Meteor.call("thoughts.remove", thought._id)
-                          }
-                        />
-                        <IconButton
-                          children={thought.done ? "🛑" : "✅"}
-                          onClick={() =>
-                            Meteor.call("thoughts.update", thought._id, {
-                              done: !thought.done,
-                              updatedAt: Date.now(),
-                            })
-                          }
-                        />
-                      </>
-                    }
-                    {...thought}
-                  />
-                ))}
-          </Box>
-        </Grid>
-      )}
-    </AppContext.Consumer>
-  );
-}
+        </Box>
+      </Grid>
+    )}
+  </AppContext.Consumer>
+);
 
 export default App;
---
+++ imports/ui/Chapters.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Box, Flex, Heading, IconButton } from "theme-ui";
+import Cursor from "../api/chapters";
+import { Meteor } from "meteor/meteor";
+import AppContext from "./AppContext";
+import New from "./New";
+import ElementRow from "./ElementRow";
+import Word from "./Word";
+
+const Chapters = () => (
+  <AppContext.Consumer>
+    {(props) => (
+      <Box>
+        <Heading mb={3}>Chapters</Heading>
+        <New
+          clickMixin={(id, value) =>
+            wordChunks(value).map((word, index) =>
+              Meteor.call("words.insert", {
+                value: word,
+                index: index,
+                parentId: id,
+                createdAt: Date.now(),
+                updatedAt: Date.now(),
+              })
+            )
+          }
+          cursor={Cursor}
+          placeholder="Add a new chapter"
+        />
+        {props.chapters &&
+          props.chapters.map((chapter) => (
+            <ElementRow
+              key={chapter._id}
+              content={
+                <Flex>
+                  {props.words
+                    .filter((word) => word.parentId === chapter._id)
+                    .map((word) => (
+                      <Word key={word._id} words={props.words} {...word} />
+                    ))}
+                </Flex>
+              }
+              actions={
+                <IconButton
+                  children="🗑"
+                  mr={2}
+                  onClick={() =>
+                    window.confirm(
+                      "Are you sure you want to delete this chapter? This action can't be undone."
+                    ) && Meteor.call("chapters.remove", chapter._id)
+                  }
+                />
+              }
+              {...chapter}
+            />
+          ))}
+      </Box>
+    )}
+  </AppContext.Consumer>
+);
+
+export default Chapters;
